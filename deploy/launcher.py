2d0260d2fd439560908dc5c5b6d6cca53035e99f
renardeinside <polarpersonal@gmail.com>
2020-05-31 13:28:19 +0200

    refactor launcher

--- deploy/launcher.py
+++ deploy/launcher.py
@@ -4,6 +4,7 @@ import os
 from argparse import ArgumentParser
 from typing import Dict, Any
 
+from databricks_cli.dbfs.api import DbfsApi
 from databricks_cli.runs.api import RunsApi
 from databricks_cli.sdk.api_client import ApiClient
 
@@ -13,19 +14,27 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 DB_HOST = os.environ['DATABRICKS_HOST']
 DB_TOKEN = os.environ['DATABRICKS_TOKEN']
 
+BRANCH_NAME = os.environ['BRANCH_NAME']
+BUILD_NUMBER = os.environ['BUILD_NUMBER']
+
 
 def get_parser():
     p = ArgumentParser(description="Simple and programmable Databricks jobs launcher")
-    p.add_argument("--branch", type=str, required=True)
-    p.add_argument("--build-number", type=str, required=True)
+    p.add_argument("--jar", type=str, required=True)
     p.add_argument("--json-file", type=str, required=True)
-    p.add_argument("--trace", type=bool)
+    p.add_argument("--trace", type=bool, action='store_true')
     return p
 
 
-def submit(client: ApiClient, job_conf: Dict[str, Any], trace: bool):
-    logging.info("Submitting job with configuration %s" % job_conf)
+def deploy(client: ApiClient, jar: str, job_conf: Dict[str, Any], trace: bool):
+    dbfs_new_jar_name = job_conf['libraries']['jar']
+    logging.info("Submitting job with configuration %s and jar file %s" % (job_conf, dbfs_new_jar_name))
+
+    dbfs_api = DbfsApi(client)
     runs_api = RunsApi(client)
+
+    dbfs_api.cp(recursive=False, overwrite=True, src=jar, dst=dbfs_new_jar_name)
+
     run_id = runs_api.submit_run(job_conf)
     logging.info("Job submitted with run id: %s" % run_id)
     if trace:
@@ -44,4 +53,5 @@ if __name__ == '__main__':
 
     job_configuration = load_conf(parsed_args["json_file"])
     db_client = ApiClient(host=DB_HOST, token=DB_TOKEN)
-    submit(db_client, job_configuration, parsed_args["trace"])
+
+    deploy(db_client, parsed_args["jar"], job_configuration, parsed_args["trace"])
