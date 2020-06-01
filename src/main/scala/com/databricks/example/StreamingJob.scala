33818aea86252a8529cdc6cb04e68adc887b7bdf
renardeinside <polarpersonal@gmail.com>
2020-06-01 21:28:28 +0200

    add stopping options for streams with timeout

--- src/main/scala/com/databricks/example/StreamingJob.scala
+++ src/main/scala/com/databricks/example/StreamingJob.scala
@@ -3,6 +3,7 @@ package com.databricks.example
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, floor, rand}
+import org.apache.spark.sql.streaming.Trigger
 
 object StreamingJob extends App with Logging {
 
@@ -49,6 +50,7 @@ object StreamingJob extends App with Logging {
     .writeStream
     .format("delta")
     .option("checkpointLocation", conf.checkpoint_location)
+    .trigger(Trigger.Once())
     .start(conf.output_path)
 
 
@@ -58,8 +60,11 @@ object StreamingJob extends App with Logging {
       writerQuery.awaitTermination()
     case Some(timeout) =>
       log.info(s"Starting bounded streaming job with timeout $timeout")
-      spark.streams.awaitAnyTermination(timeout)
+      writerQuery.awaitTermination(timeout)
+      log.info("Timeout requirement fulfilled, gracefully stopping the stream")
+      spark.streams.active.foreach(_.stop())
+      log.info("All streams stopped")
   }
 
-
+  log.info("Streaming job successfully stopped!")
 }
